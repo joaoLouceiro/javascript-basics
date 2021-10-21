ff36c19e220c1b9b9e70310ebeff50a8cf37d525
joao.louceiro <joao.louceiro@accenture.com>
2021-10-21 15:22:45 +0100

    Finished ThisKeyword

--- Percipio/ThisKeyword/ThisKeyword.js
+++ Percipio/ThisKeyword/ThisKeyword.js
@@ -7,9 +7,42 @@ var age = 35;
 console.log("window.age ", window.age);
 console.log("this.age ", this.age);
 
-//No contexto global, this === window
+//In the global context, this === window
 
 let someFunction = () => this;
 console.log("this returned from an arrow function: ", someFunction());
 
 //Arrow functions inherit the scope from their parent
+
+
+let myCar = {
+    make: "Volvo",
+    model: "s60",
+    printThis() {
+        console.log("this from myCar: ", this);
+    },
+    printDetails() {
+        console.log(`
+        Make: ${this.make}
+        Model: ${this.model}`);
+    },
+    engine: {
+        cylinders: 4,
+        horsepower: 250,
+        printThis() {
+            console.log("this from engine: ", this);
+        },
+        printDetails() {
+            console.log(`
+                Cylinders: ${this.cylinders}
+                Horsepower: ${this.horsepower}
+            `)
+        }
+    }
+}
+
+myCar.printThis();
+myCar.printDetails();
+
+myCar.engine.printThis();
+myCar.engine.printDetails();
