c146de19aece851926c64a98632d8a58e7402393
joao.louceiro <joao.louceiro@accenture.com>
2021-11-03 11:32:21 +0000

    Removed whitespaces from folder names

---
+++ Percipio/Web_Programmer_to_Apprentice_Programmer/Track_2_Web_Apps_Developer/React/28_CommentApp/comment-app/src/Comment.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import message from './message.png';
+import trash from './trash.png'
+
+export default class Comment extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
